3ec975bffccef1ea4b45841922338a450525e122
Jose E Baez <101849223+Jbr19@users.noreply.github.com>
2022-06-28 02:32:02 -0400

    Add files via upload

--- validacion.js
+++ validacion.js
@@ -1,28 +1,46 @@
 // Validar formulario
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-  });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    var nombre = document.getElementById('name').value;
-    if(nombre.length > 50 ) {
-      alert('Campo Nombre no debe estar en blanco o vacío');
-      return;
-    }
-    
-    var asunto = document.getElementById('asunto').value;
-    if (asunto.length > 50) {
-      alert('Campo Asunto no debe estar en blanco o vacío');
-      return;
-    }
 
-    var txt = document.getElementById('mensage').value;
-    if (txt.length > 300) {
-      alert('Campo Asunto no debe estar en blanco o vacío');
-      return;
-    }
-    this.submit();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("form").addEventListener('submit', validarFormulario); 
+});
+
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+  var usuario = document.getElementById('nombre').value;
+  if(usuario.length > 25) {
+    Swal.fire({
+      title: "Error al digital el nombre",
+      text: 'Puede contener un máximo de 25 caracteres',
+      icon: 'error',
+      confirmButtonText: "Continuar"
+    });
+    return;
   }
 
+  var clave = document.getElementById('asunto').value;
+  if (clave.length > 50) {
+    Swal.fire({
+      title: "Error al escribir el asunto",
+      text: "Puede contener un máximo de 50 caracteres",
+      icon: "error",
+      confirmButtonText: "Continuar"
+    });
+    return;
+  }
 
+  var clave = document.getElementById('mensage').value;
+  if (clave.length > 300) {
+    Swal.fire({
+      title: "Error en el mensaje",
+      text: "Puede contener un máximo de 300 caracteres",
+      icon: "error",
+      confirmButtonText: "Continuar"
+    });
+    return;
+  }
+
+  this.submit();
+}
